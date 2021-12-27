6d1c431311ab655bc08c403ddb12e9054d0cb75f
prajvalhl <prajvalhl@gmail.com>
2021-12-27 20:39:16 +0530

    refactor: updated component

--- src/App.js
+++ src/App.js
@@ -147,6 +147,7 @@ function App() {
                 );
                 setInput("");
                 setDateTimeInput("");
+                setComment("");
               }}
             >
               Add Task
