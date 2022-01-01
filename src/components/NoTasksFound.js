c246a209b25f5a8aed8092b951885b6710b999f4
prajvalhl <prajvalhl@gmail.com>
2022-01-01 13:07:27 +0530

    refactor: added component

---
+++ src/components/NoTasksFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NoTasksFound() {
+  return (
+    <div>
+      <h1>Your task list is empty, Start writing to add</h1>
+    </div>
+  );
+}
+
+export default NoTasksFound;
